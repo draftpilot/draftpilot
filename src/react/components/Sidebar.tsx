ea29334d5a2b74664243d0fd52cd2fd7e948c5f9
Tim Su <timsu@users.noreply.github.com>
2023-04-14 23:18:36 -0700

    Improve styling of past sessions ux

--- src/react/components/Sidebar.tsx
+++ src/react/components/Sidebar.tsx
@@ -10,30 +10,31 @@ export default () => {
     messageStore.loadSessions()
   }, [])
 
-  const loadSession = (id: string) => {
-    messageStore.loadSession(id)
-  }
-
   return (
-    <div className="p-4 flex flex-col gap-2">
-      <button className="p-2 rounded border-2 border-gray-600 cursor-pointer hover:bg-gray-400 text-gray-700">
+    <div className="p-4 flex flex-col gap-2 overflow-y-auto">
+      <button
+        onClick={() => messageStore.newSession()}
+        className="p-2 rounded border-2 border-gray-600 cursor-pointer hover:bg-gray-400 text-gray-700"
+      >
         New Session
       </button>
 
       {sessions.length && <div className="my-2">Past Sessions</div>}
 
       {sessions.map((session) => (
-        <button
-          key={session.id}
-          className={
-            'p-2 rounded border-2 border-gray-600 cursor-pointer hover:bg-gray-400 text-gray-700 ' +
-            (session.id === activeSession?.id ? 'bg-blue-100' : '')
-          }
-          disabled={session.id === activeSession?.id}
-          onClick={() => loadSession(session.id)}
-        >
-          {session.name}
-        </button>
+        <div key={session.id} className="flex flex-col">
+          <button
+            className={
+              'p-2 rounded border-2 border-gray-600 cursor-pointer hover:bg-gray-400 text-gray-700 ' +
+              (session.id === activeSession?.id ? 'bg-blue-100' : '')
+            }
+            disabled={session.id === activeSession?.id}
+            onClick={() => messageStore.loadSession(session.id)}
+          >
+            {session.name}
+          </button>
+          <div className="text-xs text-gray-500 m-2">{new Date(session.id).toLocaleString()}</div>
+        </div>
       ))}
     </div>
   )
