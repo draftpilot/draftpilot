641ad165ba6ebe88ab4635dace5809080bd95f9e
Tim Su <timsu@users.noreply.github.com>
2023-04-21 15:52:57 -0700

    Extract crashpilot to its own thing

--- src/directors/fullServiceDirector.ts
+++ src/directors/fullServiceDirector.ts
@@ -13,6 +13,7 @@ import { ChatMessage, Intent, MessagePayload, PostMessage } from '@/types'
 import { log } from '@/utils/logger'
 import path from 'path'
 import { readProjectContext } from '@/context/projectContext'
+import { CrashPilot } from '@/directors/crashPilot'
 
 export class FullServiceDirector {
   interrupted = new Set<string>()
@@ -199,54 +200,13 @@ Analyze and categorize my query: `
     await this.productAssistant.runAgent(payload, postMessage)
   }
 
+  crashPilot = new CrashPilot(this.interrupted)
   useCrashPilot = async (payload: MessagePayload, postMessage: PostMessage) => {
-    const { message, history } = payload
-
-    const model = '4' // message.options?.model || '4'
-
-    const similarCode = await indexer.vectorDB.searchWithScores(message.content, 10)
-    const similarFuncs = similarCode
-      ?.filter((s) => {
-        const [doc, score] = s
-        if (score < 0.15) return false
-        return true
-      })
-      .map((s) => s[0].pageContent)
-
-    const prompt = `I will paste in a crash log or bug report written by a user or product manager.
-Your job is to figure out where the possible bug/crash is. If it can be fixed simply, propose a 
-fix in your response, including the file name and new code with a few lines of context.
-
-If it's not clear what the fix is, come up with a few possible theories, and a plan for how the
-user can test them and report back. You can help the user think of ways to reproduce the bug,
-write a simple test case to expose the bug, or other debugging techniques.
-
----
-Relevant code snippets:
-
-${similarFuncs?.join('\n\n') || 'No similar code found'}
-
----
-User Request:
-`
-
-    const attachmentBody = attachmentListToString(message.attachments)
-    const userMessage: ChatMessage = {
-      role: 'user',
-      content: prompt + message.content + attachmentBody,
+    if (payload.history.find((h) => h.intent == Intent.CRASHPILOT)) {
+      // user has already started a conversation with the crash pilot
+      this.handleDetectedIntent(Intent.COMPLEX, payload, undefined, postMessage)
+    } else {
+      this.crashPilot.useCrashPilot(payload, this.systemMessage(), postMessage)
     }
-    const messages = compactMessageHistory([...history, userMessage], model, {
-      role: 'system',
-      content: this.systemMessage(),
-    })
-
-    const response = await streamChatWithHistory(messages, model, postMessage)
-
-    const responseMessage: ChatMessage = {
-      role: 'assistant',
-      content: response,
-    }
-
-    postMessage(responseMessage)
   }
 }
