b34998f9440b43935c1642564619a3484db5a15a
Tim Su <timsu@users.noreply.github.com>
2023-04-25 18:03:35 -0700

    Let you work the diff

--- src/react/components/FileEditMessage.tsx
+++ src/react/components/FileEditMessage.tsx
@@ -7,12 +7,17 @@ import { Fragment, useEffect, useRef, useState } from 'react'
 import MarkdownParser from './MarkdownParser'
 import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer'
 import { API } from '@/react/api/api'
-import { applyOps } from '@/utils/editOps'
+import { Op, applyOps } from '@/utils/editOps'
 import Loader from '@/react/components/Loader'
 import Button from '@/react/components/Button'
 
+type DiffState = 'accepted' | 'rejected' | undefined
+
+type DiffMap = { [file: string]: DiffState }
+
 export default function FileEditMessage({ message }: { message: ChatMessage }) {
   const content = message.content
+  const [diffMap, setDiffMap] = useState<DiffMap>({})
 
   const jsonStart = content.indexOf('{')
   const preContent = content.slice(0, jsonStart).replace(/```.*/g, '').trim()
@@ -33,7 +38,16 @@ export default function FileEditMessage({ message }: { message: ChatMessage }) {
           Error parsing JSON content
         </div>
       )}
-      {json && Object.keys(json).map((key) => <DiffContent key={key} file={key} ops={json[key]} />)}
+      {json &&
+        Object.keys(json).map((key) => (
+          <DiffContent
+            key={key}
+            file={key}
+            ops={json[key]}
+            state={diffMap[key]}
+            setState={(state) => setDiffMap({ ...diffMap, [key]: state })}
+          />
+        ))}
       <TextContent content={postContent} />
     </div>
   )
@@ -49,9 +63,20 @@ function TextContent({ content }: { content: string }) {
   )
 }
 
-function DiffContent({ file, ops }: { file: string; ops: any[] }) {
+function DiffContent({
+  file,
+  ops,
+  state,
+  setState,
+}: {
+  file: string
+  ops: Op[]
+  state: DiffState
+  setState: (state: DiffState) => void
+}) {
   const [oldCode, setOldCode] = useState<string | null>(null)
   const [newCode, setNewCode] = useState<string | null>(null)
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     API.loadFile(file).then((res) => {
@@ -64,21 +89,49 @@ function DiffContent({ file, ops }: { file: string; ops: any[] }) {
 
   return (
     <div>
-      <div className="diff-view shadow-md text-xs flex-1 max-w-full overflow-x-auto">
-        {!oldCode || (!newCode && <Loader />)}
-        {oldCode && newCode && (
-          <ReactDiffViewer
-            oldValue={oldCode}
-            newValue={newCode}
-            compareMethod={DiffMethod.LINES}
-            renderContent={HighlightedCode}
-            leftTitle={file}
-            splitView={false}
-          />
+      {(!state || open) && (
+        <div className="diff-view mb-4 shadow-md text-xs flex-1 max-w-full overflow-x-auto">
+          {!oldCode || (!newCode && <Loader />)}
+          {oldCode && newCode && (
+            <ReactDiffViewer
+              oldValue={oldCode}
+              newValue={newCode}
+              compareMethod={DiffMethod.LINES}
+              renderContent={HighlightedCode}
+              leftTitle={file}
+              splitView={false}
+            />
+          )}
+        </div>
+      )}
+      {state && (
+        <div
+          className={`p-4 mx-auto w-[768px] max-w-full ${
+            state == 'accepted' ? 'text-green-700' : 'text-red-700'
+          }`}
+        >
+          <code>
+            {file} - changes {state}
+          </code>
+        </div>
+      )}
+      <div className="flex justify-center my-4 gap-4 mx-auto w-[768px] max-w-full">
+        {state != 'accepted' && (
+          <Button onClick={() => setState('accepted')} className="bg-blue-600">
+            Accept Diff
+          </Button>
         )}
-      </div>
-      <div className="flex justify-center my-8 bg-blue-600 mx-auto w-[768px] max-w-full">
-        <Button>Accept Diff</Button>
+        {state != 'rejected' && (
+          <Button onClick={() => setState('rejected')} className="bg-red-600">
+            Reject Diff
+          </Button>
+        )}
+        {state && (
+          <Button onClick={() => setOpen(!open)} className="bg-gray-600">
+            Toggle Diff
+          </Button>
+        )}
+        <Button className="bg-gray-600">Edit File</Button>
       </div>
     </div>
   )
