094ff46d5dfedc8b16f7df1829e1deb422575151
Tim Su <timsu@users.noreply.github.com>
2023-04-28 22:58:12 -0700

    Get auto planner with looping implemented

--- src/prompts/index.ts
+++ src/prompts/index.ts
@@ -1,9 +1,12 @@
-import autoPilotPlanner from './autoPilotPlanner.ets.ts'
+import autoPilotPlanner1 from './autoPilotPlanner1.ets.ts'
+import autoPilotPlanner2 from './autoPilotPlanner2.ets.ts'
+import autoPilotPlanner3 from './autoPilotPlanner3.ets.ts'
 import chatPilot from './chatPilot.ets.ts'
 import crashPilot from './crashPilot.ets.ts'
 import draftPilot from './draftPilot.ets.ts'
 import editPilot from './editPilot.ets.ts'
 import genContext from './genContext.ets.ts'
+import jsonFixer from './jsonFixer.ets.ts'
 import systemMessage from './systemMessage.ets.ts'
 import validator from './validator.ets.ts'
 
@@ -13,7 +16,10 @@ export default {
   draftPilot,
   crashPilot,
   editPilot,
+  jsonFixer,
   validator,
   genContext,
-  autoPilotPlanner,
+  autoPilotPlanner1,
+  autoPilotPlanner2,
+  autoPilotPlanner3,
 }
