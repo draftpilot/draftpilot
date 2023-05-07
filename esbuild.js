484da41801305aeab604676c56b3af56534e6b2e
Tim Su <timsu@users.noreply.github.com>
2023-05-06 22:19:10 -0700

    Fix esbuild issue with requires in cjs dependencies

--- esbuild.js
+++ esbuild.js
@@ -1,39 +1,44 @@
 import esbuild from 'esbuild'
 import { nodeExternalsPlugin } from 'esbuild-node-externals'
 import FastGlob from 'fast-glob'
+import { commonjs } from '@hyrious/esbuild-plugin-commonjs'
 
-const mainConfig = {
-  entryPoints: ['src/main.ts'],
-  bundle: true,
-  outdir: 'dist',
-  platform: 'node',
-  target: 'node14',
-  plugins: [nodeExternalsPlugin()],
-  format: 'esm',
-  sourcemap: true,
-}
-
-const testEntries = await FastGlob('test/**/*.ts')
-
-const testConfig = {
-  entryPoints: testEntries,
-  bundle: true,
-  outdir: 'dist/test',
-  platform: 'node',
-  target: 'node14',
-  plugins: [nodeExternalsPlugin()],
-  format: 'esm',
-  sourcemap: true,
-}
-
-async function build(config) {
-  if (process.argv.includes('--watch')) {
-    const context = await esbuild.context(config)
-    await context.watch()
-  } else {
-    await esbuild.build(config)
+async function main() {
+  const mainConfig = {
+    entryPoints: ['src/main.ts'],
+    bundle: true,
+    outdir: 'dist',
+    platform: 'node',
+    target: 'node14',
+    plugins: [nodeExternalsPlugin(), commonjs()],
+    sourcemap: true,
+    format: 'esm',
   }
+
+  const testEntries = await FastGlob('test/**/*.ts')
+
+  const testConfig = {
+    entryPoints: testEntries,
+    bundle: true,
+    outdir: 'dist/test',
+    platform: 'node',
+    target: 'node14',
+    plugins: [nodeExternalsPlugin(), commonjs()],
+    sourcemap: true,
+    format: 'esm',
+  }
+
+  async function build(config) {
+    if (process.argv.includes('--watch')) {
+      const context = await esbuild.context(config)
+      await context.watch()
+    } else {
+      await esbuild.build(config)
+    }
+  }
+
+  build(mainConfig)
+  build(testConfig)
 }
 
-build(mainConfig)
-build(testConfig)
+main()
